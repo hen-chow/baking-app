121bb47863e47901e998fd6c2a378e6920dd19b4
Henrietta Chow <henchow@gmail.com>
2017-03-20 14:44:35 +1100

    first deploy to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   resources :reviews, only: [:create, :new, :edit, :show, :update, :destroy]
 
   resources :users, only: [:new, :create, :show]
+  get "signin", to: "sessions#new", as: "signin"
+  post "signin", to: "sessions#create"
+  get "signout", to: "sessions#destroy", as: "signout"
 end
